88e1ab3b4ad2209596d8e97b1550d7b579dea7f0
LynnHo <lynn_ho@163.com>
2017-08-10 15:24:07 +0800

    ...

---
+++ models_mnist.py
@@ -0,0 +1,56 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import ops
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+from functools import partial
+
+conv = partial(slim.conv2d, activation_fn=None, weights_initializer=tf.truncated_normal_initializer(stddev=0.02))
+dconv = partial(slim.conv2d_transpose, activation_fn=None, weights_initializer=tf.random_normal_initializer(stddev=0.02))
+fc = partial(ops.flatten_fully_connected, activation_fn=None, weights_initializer=tf.random_normal_initializer(stddev=0.02))
+relu = tf.nn.relu
+lrelu = partial(ops.leak_relu, leak=0.2)
+batch_norm = partial(slim.batch_norm, decay=0.9, scale=True, epsilon=1e-5, updates_collections=None)
+ln = slim.layer_norm
+
+
+def generator(z, dim=64, reuse=True, training=True):
+    bn = partial(batch_norm, is_training=training)
+    dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+
+    with tf.variable_scope('generator', reuse=reuse):
+        y = fc_bn_relu(z, 1024)
+        y = fc_bn_relu(y, 7 * 7 * dim * 2)
+        y = tf.reshape(y, [-1, 7, 7, dim * 2])
+        y = dconv_bn_relu(y, dim * 2, 5, 2)
+        img = tf.tanh(dconv(y, 1, 5, 2))
+        return img
+
+
+def discriminator(img, dim=64, reuse=True, training=True):
+    bn = partial(batch_norm, is_training=training)
+    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+    fc_bn_lrelu = partial(fc, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+
+    with tf.variable_scope('discriminator', reuse=reuse):
+        y = lrelu(conv(img, 1, 5, 2))
+        y = conv_bn_lrelu(y, dim, 5, 2)
+        y = fc_bn_lrelu(y, 1024)
+        logit = fc(y, 1)
+        return logit
+
+
+def discriminator_wgan_gp(img, dim=64, reuse=True, training=True):
+    conv_ln_lrelu = partial(conv, normalizer_fn=ln, activation_fn=lrelu, biases_initializer=None)
+    fc_ln_lrelu = partial(fc, normalizer_fn=ln, activation_fn=lrelu, biases_initializer=None)
+
+    with tf.variable_scope('discriminator', reuse=reuse):
+        y = lrelu(conv(img, 1, 5, 2))
+        y = conv_ln_lrelu(y, dim, 5, 2)
+        y = fc_ln_lrelu(y, 1024)
+        logit = fc(y, 1)
+        return logit
